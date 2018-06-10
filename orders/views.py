79e4317d7f8d0eb87f0eca4d64551beb614f0960
WEB DEWE <deweweb@gmail.com>
2018-06-10 19:02:02 +0200

    184 ajax save delete db

--- orders/views.py
+++ orders/views.py
@@ -45,16 +45,36 @@ def basket_adding(request):
     return_dict = dict()
     session_key = request.session.session_key
     print(request.POST)
-
     data = request.POST
     product_id = data.get("product_id")
     numb = data.get("numb")
+    is_delete = data.get("is_delete")
+    print(is_delete)
 
+    if is_delete == 'true':
+        ProductInBusket.objects.filter(id=product_id).update(is_active=False)
+    else:
+        new_product, created = ProductInBusket.objects.get_or_create(session_key=session_key, product_id=product_id, is_active=True, defaults={"number": numb})
+        print(new_product)
+        if not created:
+            print("not created")
+            new_product.number += int(numb)
+            new_product.save(force_update=True)
 
-    new_product = ProductInBusket.objects.create(session_key=session_key, product_id=product_id, number=numb)
-
-    products_total_numb = ProductInBusket.objects.filter(session_key=session_key, is_active=True).count()
+    # common code for two cases
+    products_in_basket = ProductInBusket.objects.filter(session_key=session_key, is_active=True)
+    products_total_numb = products_in_basket.count()
     return_dict["products_total_numb"] = products_total_numb
+
+    return_dict["products"] = list()
+
+    for item in products_in_basket:
+        product_dict = dict()
+        product_dict["id"] = item.id
+        product_dict["name"] = item.product.name
+        product_dict["price_per_item"] = item.price_per_item
+        product_dict["numb"] = item.number
+        return_dict["products"].append(product_dict)
 
     return JsonResponse(return_dict)
 
--- static/static_dev/js/scripts.js
+++ static/static_dev/js/scripts.js
@@ -1,6 +1,45 @@
 $(document).ready(function(){
     var form = $('#form_buying_product');
     console.log(form);
+
+    function basketUpdating(product_id, numb, is_delete){
+        var data = {};
+        data.product_id = product_id;
+        data.numb = numb;
+        var carf_token = $('#form_buying_product [name = "csrfmiddlewaretoken"]').val();
+        data["csrfmiddlewaretoken"] = carf_token;
+
+        if (is_delete) {
+            data["is_delete"] = true
+        }
+
+        var url = form.attr("action");
+        console.log("DATA" + data);
+        $.ajax({
+            url: url,
+            type: 'POST',
+            data: data,
+            cache: true,
+            success: function (data) {
+                console.log("OK");
+                console.log(data.products_total_numb)
+                if (data.products_total_numb || data.products_total_numb == 0){
+                    $('#basket_total_number').text("(" + data.products_total_numb + ")")
+                    console.log(data.products);
+                    $('.basket-items ul').html("");
+                    $.each(data.products, function(k, y){
+                    $('.basket-items ul').append('<li>'+ y.name + ', ' + y.numb
+                        + ' itm. for ' + y.price_per_item
+                        + ' €  ' + '<a href="" class="delete-item" data-product_id="'+ y.id + '">' +  'X'  + '</a>' + '</li>');
+                    })
+                }
+            },
+            error: function(data) {
+                console.log("ERROR");
+            }
+        })
+    }
+
     form.on('submit', function(e){
         e.preventDefault();
         console.log('123');
@@ -10,12 +49,14 @@ $(document).ready(function(){
         var product_id = submit_btn.data("product_id");
         var product_name = submit_btn.data("name");
         var product_price = submit_btn.data("price");
-        var product_image = submit_btn.data("name");
+
         console.log(product_id);
         console.log(product_name);
         console.log(product_price);
 
-        var data = {};
+        basketUpdating(product_id, numb, is_delete=false)
+
+/*        var data = {};
         data.product_id = product_id;
         data.numb = numb;
 
@@ -35,16 +76,24 @@ $(document).ready(function(){
                 console.log(data.products_total_numb)
                 if (data.products_total_numb){
                     $('#basket_total_number').text("(" + data.products_total_numb + ")")
+                    console.log(data.products);
+                    $('.basket-items ul').html("");
+                    $.each(data.products, function(k,y){
+                    $('.basket-items ul').append('<li>'+ product_name + ', ' + numb
+                        + ' itm. for ' + product_price
+                        + ' €  ' + '<a href="" class="delete-item">' +  'X'  + '</a>' + '</li>');
+                    })
                 }
             },
             error: function() {
                 console.log("ERROR");
             }
-        })
+        })*/
 
 
-        $('.basket-items ul').append('<li>'+ product_name + ', ' + numb
-            + ' itm. for ' + product_price + ' €  ' + '<a href="" class="delete-item">' +  'X'  + '</a>' + '</li>');
+/*        $('.basket-items ul').append('<li>'+ product_name + ', ' + numb
+            + ' itm. for ' + product_price
+            + ' €  ' + '<a href="" class="delete-item">' +  'X'  + '</a>' + '</li>');*/
     })
 
     function shownigBasket() {
@@ -67,7 +116,10 @@ $(document).ready(function(){
 
     $(document).on('click', '.delete-item', function(e){
         e.preventDefault();
-        $(this).closest('li').remove();
+        //$(this).closest('li').remove();
+        product_id = $(this).data("product_id");
+        numb = 0;
+        basketUpdating(product_id, numb, is_delete=true);
     })
 
 })
